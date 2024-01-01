1b4b8483d241b0595819db03c4f7eb640980e6ef
DESKTOP-9N2P72A\LaptopAnhHiep <bohiepdzz1@gmail.com>
2024-01-01 14:12:55 +0700

    add Home UI

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import Chat from "./Screens/Chat.js";
 import Login from "./Screens/Login.js";
 import Signup from "./Screens/Signup.js";
 const Stack = createNativeStackNavigator();
-
+import Home from "./Screens/Home.js";
 function ChatStack(){
   return(
     <Stack.Navigator>
+      <Stack.Screen options={{headerShown:true}} name="Home" component={Home}/>
       <Stack.Screen options={{headerShown:false}} name="Signup" component={Signup}/>
     </Stack.Navigator>
   
